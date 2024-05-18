1de0da40bcd64b49c933c5b40b67153a399e6eca
김용민 <dydals3440@gmail.com>
2024-05-18 20:12:01 +0900

    feat: React 한 li 안에 title/desc 모두 있게 처리 tabMenu

--- app/components/tabMenu/3_r.tsx
+++ app/components/tabMenu/3_r.tsx
@@ -2,69 +2,41 @@ import { useState } from 'react';
 import cx from './cx';
 import data from './data';
 
-{
-	/* <div>
-	<tabList>
-		<tab>....</tab>
-		<tab>....</tab>
-		<tab>....</tab>
-		<tab>....</tab>
-	</tabList>
-	<description>...</description>
-	<description>...</description>
-	<description>...</description>
-	<description>...</description>
-</div> */
-}
-
-{
-	/* <ul>
-	<item>
-		<tab></tab>
-		<description></description>
-	</item>
-	<item>
-		<tab></tab>
-		<description></description>
-	</item>
-	<item>
-		<tab></tab>
-		<description></description>
-	</item>
-</ul> */
-}
-
 const TabItem = ({
 	id,
 	title,
+	description,
 	current,
 	toggle,
 }: {
 	id: string;
 	title: string;
+	description: string;
 	current: boolean;
 	toggle: () => void;
 }) => {
 	return (
-		<li className={cx('tab', { current })} key={id} onClick={toggle}>
-			{title}
+		<li className={cx('item', { current })} key={id}>
+			<div className={cx('tab')} onClick={toggle}>
+				{title}
+			</div>
+			<div className={cx('description')}>{description}</div>
 		</li>
 	);
 };
 
 const TabMenu3 = () => {
-	const [currentId, setCurrentId] = useState<string | null>(data[0].id);
+	const [currentId, setCurrentId] = useState<string>(data[0].id);
 
 	const toggleItem = (id: string) => () => {
-		setCurrentId((prev) => (prev === id ? null : id));
+		setCurrentId(id);
 	};
-
 	return (
 		<>
 			<h3>
 				#3. React<sub>한 li 안에 title/desc 모두 있게 처리</sub>
 			</h3>
-			<div className={cx('container', 'tabMenu2')}>
+			<ul className={cx('container', 'tabMenu3')}>
 				{data.map((d) => (
 					<TabItem
 						{...d}
@@ -73,7 +45,7 @@ const TabMenu3 = () => {
 						toggle={toggleItem(d.id)}
 					/>
 				))}
-			</div>
+			</ul>
 		</>
 	);
 };
